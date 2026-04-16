3da8f5e46fbfd69447f89f3f8729e12337899a94
Nayla Fairuz <fairuzay0@gmail.com>
2026-04-16 14:50:13 +0700

    Menambah Routing Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
+// Include Student Controller
+use App\Http\Controllers\StudentController;
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/student');
 });
+
+Route::get('/student', [StudentController::class, 'index'])
+    ->name('student.index');
+
+Route::get('/student/add', [StudentController::class, 'create'])
+    ->name('student.create');
+
+Route::POST('/student/add', [StudentController::class, 'store'])
+    ->name('student.store');
+
+Route::get('/student/{id}', [StudentController::class, 'show'])
+    ->name('student.show');
+
+Route::get('/student/edit/{id}', [StudentController::class, 'edit'])
+    ->name('student.edit');
+
+Route::PUT('/student/edit/{id}', [StudentController::class, 'update'])
+    ->name('student.update');
+
+Route::DELETE('/student/delete/{id}', [StudentController::class, 'destroy'])
+    ->name('student.destroy');
